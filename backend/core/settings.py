6e028a8fd7753b9128219a73c364d30a1a571d0e
sunny <sunnysharma81871@gmail.com>
2025-11-27 22:11:53 +0530

    Fix database settings

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -99,18 +99,27 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
-        conn_max_age=600
-    )
-}
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
+#     'default': dj_database_url.config(
+#         default='sqlite:///db.sqlite3',
+#         conn_max_age=600
+#     )
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+# 2. Agar Render par DATABASE_URL milta hai, to use override karein
+database_url = os.environ.get("DATABASE_URL")
+
+if database_url and database_url.startswith("postgres"):
+    DATABASES['default'] = dj_database_url.parse(
+        database_url,
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 
 
 # Password validation
